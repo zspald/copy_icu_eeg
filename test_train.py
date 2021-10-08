7773247628a09959d733880a11b9d101145c8113
zspald <spaldingzac@gmail.com>
2021-10-07 20:35:52 -0400

    preparing 5 fold cv for cnn training

--- test_train.py
+++ test_train.py
@@ -9,17 +9,44 @@ from train import EEGLearner
 #                 "ICUDataRedux_0068", "ICUDataRedux_0069", "ICUDataRedux_0072", "ICUDataRedux_0073", "ICUDataRedux_0074", 
 #                 "ICUDataRedux_0078", "ICUDataRedux_0082", "ICUDataRedux_0083", "ICUDataRedux_0084", "ICUDataRedux_0085", 
 #                 "ICUDataRedux_0086", "ICUDataRedux_0087", "ICUDataRedux_0089", "ICUDataRedux_0090", "ICUDataRedux_0091"]
-patient_list_cv_false = ["CNT684", "CNT687", "CNT689", "CNT690", "CNT691", "CNT692", "CNT694", "CNT695",
-                         "CNT698", "CNT700", "CNT701", "CNT702", "CNT705", "CNT706", "ICUDataRedux_0054", "ICUDataRedux_0061",
-                         "ICUDataRedux_0063", "ICUDataRedux_0064", "ICUDataRedux_0065", "ICUDataRedux_0066",
-                         "ICUDataRedux_0068", "ICUDataRedux_0069", "ICUDataRedux_0072", "ICUDataRedux_0073", "ICUDataRedux_0074",
-                         "ICUDataRedux_0078", "ICUDataRedux_0082", "ICUDataRedux_0083", "ICUDataRedux_0084",
-                         "ICUDataRedux_0086", "ICUDataRedux_0087", "ICUDataRedux_0089", "ICUDataRedux_0090", "ICUDataRedux_0091"]
+# patient_list_cv_false = ["CNT684", "CNT687", "CNT689", "CNT690", "CNT691", "CNT692", "CNT694", "CNT695",
+#                          "CNT698", "CNT700", "CNT701", "CNT702", "CNT705", "CNT706", "ICUDataRedux_0054", "ICUDataRedux_0061",
+#                          "ICUDataRedux_0063", "ICUDataRedux_0064", "ICUDataRedux_0065", "ICUDataRedux_0066",
+#                          "ICUDataRedux_0068", "ICUDataRedux_0069", "ICUDataRedux_0072", "ICUDataRedux_0073", "ICUDataRedux_0074",
+#                          "ICUDataRedux_0078", "ICUDataRedux_0082", "ICUDataRedux_0083", "ICUDataRedux_0084",
+#                          "ICUDataRedux_0086", "ICUDataRedux_0087", "ICUDataRedux_0089", "ICUDataRedux_0090", "ICUDataRedux_0091"]
+
+pt_list_nsz = np.array(['CNT684', 'CNT685', 'CNT687', 'CNT688', 'CNT689', 'CNT690', 'CNT691', 
+                        'CNT692', 'CNT694', 'CNT695', 'CNT698', 'CNT700', 'CNT701', 'CNT702', 
+                        'CNT705', 'CNT706', 'CNT708', 'CNT710', 'CNT711', 'CNT713', 'CNT715', 
+                        'CNT720', 'CNT723', 'CNT724', 'CNT725', 'CNT726', 'CNT729', 'CNT730', 
+                        'CNT731', 'CNT732', 'CNT733', 'CNT734', 'CNT737', 'CNT740', 'CNT741', 
+                        'CNT742', 'CNT743', 'CNT748', 'CNT750', 'CNT757', 'CNT758', 'CNT765', 
+                        'CNT773', 'CNT774', 'CNT775', 'CNT776', 'CNT778', 'CNT782', 
+                        'ICUDataRedux_0023', 'ICUDataRedux_0026', 'ICUDataRedux_0029',
+                        'ICUDataRedux_0030', 'ICUDataRedux_0034', 'ICUDataRedux_0035', 
+                        'ICUDataRedux_0043', 'ICUDataRedux_0044', 'ICUDataRedux_0047', 
+                        'ICUDataRedux_0048'])
+
+pt_list_sz = np.array(['ICUDataRedux_0060', 'ICUDataRedux_0061', 'ICUDataRedux_0062',
+                       'ICUDataRedux_0063', 'ICUDataRedux_0064', 'ICUDataRedux_0065',
+                       'ICUDataRedux_0066', 'ICUDataRedux_0067', 'ICUDataRedux_0068',
+                       'ICUDataRedux_0069', 'ICUDataRedux_0072', 'ICUDataRedux_0073',
+                       'ICUDataRedux_0074', 'ICUDataRedux_0054', 'ICUDataRedux_0078',
+                       'ICUDataRedux_0082', 'ICUDataRedux_0083', 'ICUDataRedux_0084',
+                       'ICUDataRedux_0085', 'ICUDataRedux_0086', 'ICUDataRedux_0087',
+                       'ICUDataRedux_0089', 'ICUDataRedux_0090', 'ICUDataRedux_0091',
+                       'ICUDataRedux_0003', 'ICUDataRedux_0004', 'ICUDataRedux_0006',
+                       'CNT929', 'ICUDataRedux_0027', 'ICUDataRedux_0028', 'ICUDataRedux_0033',
+                       'ICUDataRedux_0036', 'ICUDataRedux_0040', 'ICUDataRedux_0042',
+                       'ICUDataRedux_0045', 'ICUDataRedux_0049', 'ICUDataRedux_0050'])
+
+pt_list = np.r_[pt_list_nsz, pt_list_sz]
 
 print("========== Training example ==========")
 # train_module = EEGLearner(patient_list_cv_true)
-train_module = EEGLearner(patient_list_cv_false)
+train_module = EEGLearner(pt_list, pt_list_sz, pt_list_nsz)
 # print(len(train_module.patient_list))
-train_module.train_cnn(epochs=50, control=1800, cross_val=False, save=False, verbose=1)
+train_module.train_cnn(epochs=50, control=1800, kfold=True, save=True, verbose=1)
 # train_module.train_convolutional_gru(epochs=20, batch_size=100, control=1800, save=True, seq_len=20, verbose=1)
 # train_module.train_conv_lstm(epochs=20, cross_val=False, save=True, seq_len=20, verbose=1)
--- train.py
+++ train.py
@@ -13,7 +13,7 @@ import h5py
 import numpy as np
 import os
 import random
-
+from sklearn.model_selection import KFold
 
 # A class that runs a 2D convolutional neural network
 class EEGLearner:
@@ -25,12 +25,14 @@ class EEGLearner:
     #   patient_list: list of all patient IDs
     #   name: the name of the training model
     #   shape: the shape of the EEG data
-    def __init__(self, patient_list):
+    def __init__(self, patient_list, sz_list=None, nsz_list=None):
         # Reset the image ordering so that channels occupy the first dimension
         set_image_data_format('channels_first')
         # Initialize fields
         self.name = ''
         self.patient_list = patient_list
+        self.sz_list = sz_list
+        self.nsz_list = nsz_list
         self.loss_weights = {0: 1.0, 1: 2.0}
         # Check the shape of the EEG data
         file = h5py.File('data/%s_data.h5' % patient_list[0], 'r')
@@ -49,10 +51,14 @@ class EEGLearner:
     #   visualize: whether to visualize the train/validation/test results
     # Outputs
     #   model: the trained CNN model
-    def train_cnn(self, epochs, batch_size=25, control=None, cross_val=False, save=True, verbose=1, visualize=True):
+    def train_cnn(self, epochs, batch_size=25, control=None, cross_val=False, k_fold=False, save=True, verbose=1, visualize=True):
         model = EEGModel.convolutional_network(self.shape)
-        model = self.train_model(epochs, model, 'conv', batch_size=batch_size, control=control, cross_val=cross_val,
-                                 save=save, use_seq=False, verbose=verbose, visualize=visualize)
+        if k_fold:
+            model = self.train_model_kfold_cv(epochs, model, 'conv', folds=5, batch_size=batch_size, control=control, save=save, use_seq=False, 
+                                              verbose=verbose, visualize=visualize)
+        else:
+            model = self.train_model(epochs, model, 'conv', batch_size=batch_size, control=control, cross_val=cross_val,
+                                     save=save, use_seq=False, verbose=verbose, visualize=visualize)
         return model
 
     # Trains a convolutional GRU model based on the EEG dataset
@@ -114,7 +120,7 @@ class EEGLearner:
             # Initialize list containers for training history and metrics
             history_list = [None for _ in range(len(self.patient_list))]
             metric_list = [None for _ in range(len(self.patient_list))]
-            # Iterate over all patients for cross validation
+            # Iterate over all patients for cross validation (leave one out)
             for ii in range(len(self.patient_list)):
                 print('Iteration: {ii}')
                 # Initialize generators for training, validation and testing data
@@ -193,6 +199,54 @@ class EEGLearner:
                 EEGEvaluator.test_results(metrics_postprocess)
         return model
 
+    def train_model_kfold_cv(self, epochs, model, name, folds=5, batch_size=25, control=None, save=True,
+                    seq_len=20, use_seq=False, verbose=1, visualize=True):
+        # Save the name of the training model
+        self.name = name
+
+        # Initialize list containers for training history and metrics
+        history_list = [None for _ in range(len(self.patient_list))]
+        metric_list = [None for _ in range(len(self.patient_list))]
+
+        sz_split, nsz_split = get_fold_splits(self.sz_list, self.nsz_list, folds)
+
+        # Iterate over all patients for cross validation (kfold)
+        for i in range(folds):
+            print('Fold: {i}')
+            # Initialize generators for training, validation and testing data
+            train_patients, validation_patients, test_patients = self.split_data_kfold(sz_split, nsz_split, i, train_split=0.9)
+            train_generator = EEGDataGenerator(train_patients, batch_size=batch_size, control=control,
+                                                sample_len=self.length, seq_len=seq_len, use_seq=use_seq)
+            validation_generator = EEGDataGenerator(valid_patients, batch_size=batch_size, control=control,
+                                                    sample_len=self.length, seq_len=seq_len, shuffle=False,
+                                                    use_seq=use_seq)
+            test_generator = EEGDataGenerator(test_patients, batch_size=batch_size, sample_len=self.length,
+                                                seq_len=seq_len, shuffle=False, use_seq=use_seq)
+            # Load and train the model
+            history = model.fit_generator(generator=train_generator, validation_data=validation_generator,
+                                            class_weight=self.loss_weights, epochs=epochs, shuffle=True,
+                                            verbose=verbose)
+            history_list[ii] = history
+            # Save model and obtain predictions for test data
+            if save:
+                new_dir = 'model-%s' % name
+                os.mkdir('cnn_models\%s' % new_dir)
+                model.save('cnn_models\%s\%s-fold-%d.h5' % (new_dir, name, fold), save_format='h5')
+            predict = model.predict_generator(test_generator, verbose=0)
+            predict = np.argmax(predict, axis=1)
+            # Obtain annotations from the test generator
+            labels = test_generator.get_annots()
+            # Post-process the model predictions and obtain labels
+            predict = EEGEvaluator.postprocess_outputs(predict, length=self.length)
+            # Compute evaluation metrics for post-processed outputs
+            metrics_postprocess = EEGEvaluator.evaluate_metrics(labels[:, 0], predict)
+            metric_list[ii] = metrics_postprocess
+        # Display results
+        if visualize:
+            EEGEvaluator.training_curve_cv(history_list)
+            EEGEvaluator.test_results_cv(metric_list)
+
+
     # Divides training, validation and testing data
     # Inputs
     #   patient_list: list of all patient IDs
@@ -263,4 +317,37 @@ class EEGLearner:
         # Obtain the list of patients used for training, validating and testing
         train_patients = new_patient_list[:int(train_split * num)]
         validation_patients = new_patient_list[int(train_split * num):]
+        return train_patients, validation_patients, test_patients
+
+    @staticmethod
+    def get_fold_splits(sz_list, nsz_list, n_folds):
+        # create kfold objects to split sz and nsz data
+        kf_sz = KFold(n_splits=folds, shuffle=True)
+        kf_nsz = KFold(n_splits=folds, shuffle=True)
+
+        # get split indices for all 5 folds
+        sz_split = list(kf_sz.split(self.sz_list))
+        nsz_split = list(kf_nsz.split(self.nsz_list))
+
+        return sz_split, nsz_split
+
+    @staticmethod
+    def split_data_kfold(sz_split, nsz_split, curr_fold, train_split=0.9):
+        # get train split indices for ith fold
+        train_idx_sz, test_idx_sz = sz_split[i]
+        train_idx_nsz, test_idx_nsz = nsz_split[i]
+
+        # construct training set from splits of sz and nsz list
+        train_sz = pt_list_sz[train_idx_sz]
+        train_nsz = pt_list_nsz[train_idx_nsz]
+        train_list = np.r_[train_sz, train_nsz]
+
+        train_patients = train_list[:int(train_split*len(train_list))]
+        validation_patients = train_list[int(train_split*len(train_list)):]
+
+        # construct testing set from sz and nsz splits to save with model
+        test_sz = pt_list_sz[test_idx_sz]
+        test_nsz = pt_list_nsz[test_idx_nsz]
+        test_patients = np.r_[test_sz, test_nsz]
+
         return train_patients, validation_patients, test_patients
